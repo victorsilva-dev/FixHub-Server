6a7da16d8670c2ff2f7bfdd7172e23b090cc31c5
vsilvasoares1710 <vsilvasoares1710@gmail.com>
2020-02-20 12:05:11 -0300

    added the error messages for the profissionais model

--- src/app/models/Profissionais.js
+++ src/app/models/Profissionais.js
@@ -8,57 +8,76 @@ class Profissionais extends Model {
 					type: DataTypes.STRING,
 					allowNull: false,
 					validate: {
-						notEmpty: true, //não permite campo vazio
-						len: [3, 70], // determina a quantidade minima e maxima de caracteres
-						isAlpha: true // permite apenas letras,
+						notEmpty: { msg: "O campo 'nome' deve ser preenchido" },
+						len: {
+							args: [3, 70],
+							msg: "O campo 'nome' deve conter de 3 a 70 caracteres"
+						}
 					}
 				},
 				email: {
 					type: DataTypes.STRING,
 					allowNull: false,
 					validate: {
-						isEmail: true,
-						notEmpty: true //não permite campo vazio
+						isEmail: {
+							msg: "O campo 'email' deve seguir o formato email@email.com"
+						},
+						notEmpty: { msg: "O campo 'email' deve ser preenchido" }
 					}
 				},
 				cpf_cnpj: {
 					type: DataTypes.STRING,
 					allowNull: false,
 					validate: {
-						len: [11, 18], // determina a quantidade minima e maxima de caracteres
-						notEmpty: true //não permite campo vazio
-					},
+						len: {
+							args: [11, 18],
+							msg: "O campo 'cpf/cnpj' deve conter de 11 a 18 caracteres"
+						},
+						notEmpty: { msg: "O campo 'cpf/cnpj' deve ser preenchido" }
+					}
 				},
 				senha: {
 					type: DataTypes.STRING,
 					allowNull: false,
 					validate: {
-						len: [6, 10], // determina a quantidade minima e maxima de caracteres
-						notEmpty: true //não permite campo vazio
+						len: {
+							args: [6, 32],
+							msg: "O campo 'senha' deve conter de 6 a 32 caracteres"
+						},
+						notEmpty: { msg: "O campo 'senha' deve ser preenchido" }
 					}
 				},
 				estado: {
 					type: DataTypes.STRING,
 					allowNull: false,
 					validate: {
-						len: [2, 2] // determina a quantidade minima e maxima de caracteres
+						len: {
+							args: [6, 32],
+							msg: "O campo 'estado' deve conter de 2 caracteres"
+						},
+						notEmpty: { msg: "O campo 'estado' deve ser preenchido" }
 					}
 				},
 				cidade: {
 					type: DataTypes.STRING,
 					allowNull: false,
 					validate: {
-						notEmpty: true, //não permite campo vazio
-						len: [3, 30], // determina a quantidade minima e maxima de caracteres
-						isAlpha: true // permite apenas letras,
+						len: {
+							args: [3, 50],
+							msg: "O campo 'cidade' deve conter de 3 a 50 caracteres"
+						},
+						notEmpty: { msg: "O campo 'cidade' deve ser preenchido" }
 					}
 				},
 				bairro: {
 					type: DataTypes.STRING,
 					allowNull: false,
 					validate: {
-						notEmpty: true, //não permite campo vazio
-						len: [3, 30] // determina a quantidade minima e maxima de caracteres
+						len: {
+							args: [3, 50],
+							msg: "O campo 'bairro' deve conter de 3 a 50 caracteres"
+						},
+						notEmpty: { msg: "O campo 'bairro' deve ser preenchido" }
 					}
 				},
 				cep: DataTypes.STRING,
@@ -68,44 +87,59 @@ class Profissionais extends Model {
 					type: DataTypes.STRING,
 					allowNull: false,
 					validate: {
-						notEmpty: true, //não permite campo vazio
-						len: [9, 14] // determina a quantidade minima e maxima de caracteres
+						len: {
+							args: [9, 14],
+							msg: "O campo 'celular' deve conter de 9 a 14 caracteres"
+						},
+						notEmpty: { msg: "O campo 'calular' deve ser preenchido" }
 					}
 				},
 				telefone: {
 					type: DataTypes.STRING,
 					validate: {
-						len: [0, 13] // determina a quantidade minima e maxima de caracteres
+						len: {
+							args: [0, 14],
+							msg: "O campo 'telefone' deve conter até 13 caracteres"
+						}
 					}
 				},
 				whatsapp: {
 					type: DataTypes.STRING,
 					validate: {
-						len: [0, 14] // determina a quantidade minima e maxima de caracteres
+						len: {
+							args: [0, 14],
+							msg: "O campo 'whatsapp' deve conter até 13 caracteres"
+						}
 					}
 				},
 				linkedin: {
 					type: DataTypes.STRING,
-					validate: {
-						len: [0, 70] // determina a quantidade minima e maxima de caracteres
+					len: {
+						args: [0, 70],
+						msg: "O campo 'linkedin' deve conter até 70 caracteres"
 					}
 				},
 				facebook: {
 					type: DataTypes.STRING,
-					validate: {
-						len: [0, 70] // determina a quantidade minima e maxima de caracteres
+					len: {
+						args: [0, 70],
+						msg: "O campo 'facebook' deve conter até 70 caracteres"
 					}
 				},
 				site: {
 					type: DataTypes.STRING,
-					validate: {
-						len: [0, 70] // determina a quantidade minima e maxima de caracteres
+					len: {
+						args: [0, 70],
+						msg: "O campo 'site' deve conter até 70 caracteres"
 					}
 				},
 				texto_anuncio: {
 					type: DataTypes.TEXT,
 					validate: {
-						len: [0, 2000] // determina a quantidade minima e maxima de caracteres
+						len: {
+							args: [0, 3000],
+							msg: "O campo 'texto_anuncio' deve conter até 3000 caracteres"
+						}
 					}
 				},
 				tags: DataTypes.JSON,
